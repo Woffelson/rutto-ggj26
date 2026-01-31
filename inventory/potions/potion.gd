a01d72b85cbd0f6a390cca921d8b7ba5f1ef74cc
Woffelson <twingelitwongeli@hotmail.com>
2026-01-31 18:52:57 +0200

    houses yeay

--- inventory/potions/potion.gd
+++ inventory/potions/potion.gd
@@ -1,8 +1,20 @@
 class_name Potion extends Area2D
 
-enum Type {BOTTLE_A, BOTTLE_B, MUSHROOM, WORMS, ONION, BERRIES}
+enum Type {BOTTLE_A, BOTTLE_B, MUSHROOM_A, MUSHROOM_B, WORMS_A, WORMS_B, ONION_A, ONION_B, BERRIES_A, BERRIES_B}
 
 @export var type: Type
+var texts: Dictionary[Type,String] = {
+	Type.BOTTLE_A: "Tincture to cure\ncough and pleghm",
+	Type.BOTTLE_B: "",
+	Type.MUSHROOM_A: "A soothing\ntrip to calm\npain-ridden\nnerves",
+	Type.MUSHROOM_B: "",
+	Type.WORMS_A: "Instant relief for those on their last legs, suffering from mortal wounds and other ailments",
+	Type.WORMS_B: "",
+	Type.ONION_A: "For use against sniffles and flu",
+	Type.ONION_B: "",
+	Type.BERRIES_A: "Certain relief\nagainst hiccup",
+	Type.BERRIES_B: "",
+}
 
 signal selected(p: Potion)
 
