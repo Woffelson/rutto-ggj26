2e6c858a9cdf1a9f740aeddd60007d61036bbd04
Woffelson <twingelitwongeli@hotmail.com>
2026-02-01 12:57:12 +0200

    neko animu

---
+++ map/catto.gd
@@ -0,0 +1,8 @@
+extends Sprite2D
+
+@onready var og_scale: Vector2 = scale
+
+func _ready() -> void:
+	var tween: Tween = create_tween().set_loops()
+	tween.tween_property(self,"scale",og_scale+(Vector2(0.01,-0.01)),1)
+	tween.tween_property(self,"scale",scale+(Vector2(-0.01,0.01)),1)
