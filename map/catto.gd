6dde151215bd757d4af27cbdc01eb5e548030394
Woffelson <twingelitwongeli@hotmail.com>
2026-02-01 13:14:18 +0200

    graf stuf

--- map/catto.gd
+++ map/catto.gd
@@ -4,5 +4,5 @@ extends Sprite2D
 
 func _ready() -> void:
 	var tween: Tween = create_tween().set_loops()
-	tween.tween_property(self,"scale",og_scale+(Vector2(0.01,-0.01)),1)
-	tween.tween_property(self,"scale",scale+(Vector2(-0.01,0.01)),1)
+	tween.tween_property(self,"scale",og_scale+(Vector2(0.1,-0.1)),1)
+	tween.tween_property(self,"scale",scale+(Vector2(-0.1,0.1)),1)
